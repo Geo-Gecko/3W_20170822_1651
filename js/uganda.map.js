49a873aca1e2cd15724d8abdd3bef053c2ce602c
Dizux <rukuddez@gmail.com>
2017-09-11 15:38:26 +0300

    Replaced Static Legend with Dynamic Legend

--- js/uganda.map.js
+++ js/uganda.map.js
@@ -141,11 +141,11 @@
       d3.select("#d3-map-wrapper").style("height", h + "px");
     }
     var w = (window.innerWidth ||
-      document.documentElement.clientWidth ||
-      document.body.clientWidth);
-    d3.select(".list-container").style("height", h - 0 +"px")
-
-    var map = new L.Map("d3-map-container", {center: [1.367, 32.305], zoom: 7, zoomControl:false})
+       document.documentElement.clientWidth ||
+       document.body.clientWidth);
+     d3.select(".list-container").style("height", h - 0 +"px")
+	 
+	 var map = new L.Map("d3-map-container", {center: [1.367, 32.305], zoom: 7, zoomControl:false})
     .addLayer(new L.TileLayer("https://api.mapbox.com/styles/v1/gecko/cj27rw7wy001w2rmzx0qdl0ek/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2Vja28iLCJhIjoidktzSXNiVSJ9.NyDfX4V8ETtONgPKIeQmvw"));
     //temporarily disable the zoom
       map.scrollWheelZoom.disable();
@@ -154,7 +154,7 @@
       map.keyboard.disable();
       map.touchZoom.disable();
       map.dragging.disable();
-
+	 
     var wrapper = d3.select("#d3-map-wrapper");
     var width = wrapper.node().offsetWidth || 960;
     var height = wrapper.node().offsetHeight || 480; // < 480 ? h : wrapper.node().offsetHeight)  || 480;
@@ -171,13 +171,13 @@
     //d3.select("#d3-map-wrapper").selectAll("*").remove();
 
     //var svg = d3.select("#d3-map-wrapper")
-    var svg = d3.select(map.getPanes().overlayPane)
+	var svg = d3.select(map.getPanes().overlayPane)
       .append("svg")
       .attr("xmlns", "http://www.w3.org/2000/svg")
       .attr("preserveAspectRatio", "xMidYMid")
       .attr("viewBox", "0 0 " + width + " " + height)
       .attr("width", width)
-      .attr("z-index", 600)
+	  .attr("z-index", 600)
       .attr("height", height);
 
     svg.append("rect")
@@ -208,8 +208,8 @@
 
     var projection = d3.geo.mercator()
       .scale(1)
-      .translate([0, 0]); //395 width/2 930 - 2400  -2400
-    //console.log(center);
+	  .translate([0,0]); //395 width/2 930 - 2400  -2400
+
     var path = d3.geo.path()
       .projection(projection);
 
@@ -230,14 +230,14 @@
     var ugandaDistricts = g.append("g").attr("class", "uganda-districts");
 
     window.updateGeoPath = function updateGeoPath(ugandaGeoJson) {
-       //console.log(ugandaGeoJson.features[0].properties.DNAME_06);
+      // console.log("updatePath");
       ugandaPath = ugandaDistricts
         .selectAll('.district')
         .data(ugandaGeoJson.features);
       ugandaPath
         .enter()
         .append("path")
-        .attr("style","z-index:600")
+		.attr("style","z-index:600")
         .attr("style","pointer-events:all!important")
         .style("cursor", "pointer")
         .style("stroke", "#fff")
@@ -265,7 +265,7 @@
           });
         })
         .on("mousemove", function (d) {
-          
+			
           var mouse = d3.mouse(svg.node()).map(function (d) {
             return parseInt(d);
           });
@@ -275,8 +275,8 @@
               "<p><span>Sectors:</span> <b>" + d.properties._sectorList.length + "</b></p>" +
               "<p><span>Agencies:</span> <b>" + d.properties._agencyList.length + "</b></p>";
           }
-          //console.log("mousemove", str);
           tooltip.html(str);
+		  
           var box = tooltip.node().getBoundingClientRect() || {
             height: 0
           };
@@ -289,18 +289,15 @@
               box.height) + "px");
         })
         .on("mouseover", function (d) {
-          //console.log("mouseover");
           d3.select(this).style("fill", "#aaa");
         })
         .on("mouseout", function (d) {
-          //console.log("mouseout");
           d3.select(this).style("fill", d.properties._agencyList ? color(d.properties._agencyList.length) :
             "#ccc");
           tooltip.classed("d3-hide", true);
         })
         .attr("d", path)
         .on("click", function (d) {
-          //console.log("click");
           var needRemove = $(d3.select(this).node()).hasClass("d3-active"); //d3.select(this).attr("class");//d3-active
           // d3.select(this).classed("d3-active", !needRemove).style("opacity", needRemove ? opacity : 1);
           // d.properties._selected = !needRemove;
@@ -375,67 +372,76 @@
 
     }
 
-    var ugandaNeighboursPath;
+    var ugandaNeighboursPath = g.append("g")
     updateGeoPath(ugandaGeoJson);
-    var tanzaniaText;
-    var indianOcean;
-    var ugandaNeighboursText;
+    var indianOcean = g.append("g")
+    var ugandaNeighboursText = g.append("g")
     var domain = color.domain();
-
-    // var N = 4;
+    var N = 4;
     // var array = (Array.apply(null, {
     //   length: N+1
     // }).map(Number.call, Number)).map(function(d,i){
     //   return Math.round(i*(domain[1]-domain[0])/N);
     // });
-    var array = [domain[0], Math.round(2 * (domain[1] - domain[0]) / 4), Math.round(3 * (domain[1] - domain[0]) /
-      4), domain[1]];
+    var step = Math.round((domain[1] - domain[0]) / N);
+    var array = [domain[0]+Math.round(step-step/2), domain[0]+Math.round(step*2-step/2), domain[0]+Math.round(step*3-step/2), domain[0]+Math.round(step*4-step/2)];
+    var arrayLabel = [domain[0].toString() + " - " + (domain[0]+step).toString(), (domain[0]+step+1).toString() + " - " + (domain[0]+step*2).toString(), (domain[0]+step*2+1).toString() + " - " + (domain[0]+step*3).toString(), (domain[0]+step*3+1).toString() + " - " + domain[1].toString()];
+    // var array = [domain[0], Math.round(2 * (domain[1] - domain[0]) / 4), Math.round(3 * (domain[1] - domain[0]) / 4), domain[1]];//
+    // var array = [domain[0] + (domain[1] - domain[0]/2)/4, Math.round(2 * (domain[1] - domain[0]) / 4) + (domain[1] - domain[0]/2)/4, Math.round(3 * (domain[1] - domain[0]) / 4) + (domain[1] - domain[0]/2)/4, domain[1] + (domain[1] - domain[0]/2)/4];//
 
-    // var legendX = 250;
-    // var legendY = 22;
-    // svg.selectAll('.legend-rect')
-    //   .data(array)
-    //   .enter()
-    //   .append('rect')
-    //   .attr('class', 'legend-rect')
-    //   .attr("x", legendX + 20)
-    //   .attr("y", function (d, i) {
-    //     return (i + 1) * legendY + height - 735;
-    //   })
-    //   .attr("width", 20)
-    //   .attr("height", 20)
-    //   .style("stroke", "black")
-    //   .style("stroke-width", 0)
-    //   .style("fill", function (d) {
-    //     return color(d);
-    //   });
-    // //the data objects are the fill colors
+    var legendX = 250;
+    var legendY = 22;
+    svg.selectAll('.legend-rect')
+      .data(array)
+      .enter()
+      .append('rect')
+      .attr('class', 'legend-rect')
+      .attr("x", legendX + 20)
+      .attr("y", function (d, i) {
+        return (i + 1) * legendY + height - 735;
+      })
+      .attr("width", 20)
+      .attr("height", 20)
+      .style("stroke", "black")
+      .style("stroke-width", 0)
+      .style("fill", function (d) {
+        return color(d);
+      });
+    //the data objects are the fill colors
 
-    // svg.selectAll('.legend-text')
-    //   .data(array)
-    //   .enter()
-    //   .append('text')
-    //   .attr('class', 'legend-text')
-    //   .attr("x", legendX + 45)
-    //   .attr("y", function (d, i) {
-    //     return (i) * legendY + height - 710;
-    //   })
-    //   .attr("dy", "0.8em") //place text one line *below* the x,y point
-    //   .text(function (d, i) {
-    //     return d;
-    //   });
+    svg.selectAll('.legend-text')
+      .data(array)
+      .enter()
+      .append('text')
+      .attr('class', 'legend-text')
+      .attr("x", legendX + 45)
+      .attr("y", function (d, i) {
+        return (i) * legendY + height - 710;
+      })
+      .attr("dy", "0.8em") //place text one line *below* the x,y point
+      .text(function (d, i) {
+        return arrayLabel[i];
+      });
 
-    // svg.selectAll('.legend-title')
-    //   .data(["Number of Agencies"])
-    //   .enter()
-    //   .append('text')
-    //   .attr('class', 'legend-title')
-    //   .attr("x", legendX + 20)
-    //   .attr("y", height - 740)
-    //   .attr("dy", "0.8em") //place text one line *below* the x,y point
-    //   .text(function (d, i) {
-    //     return d;
-    //   });
+    svg.selectAll('.legend-title')
+      .data(["Number of Agencies"])
+      .enter()
+      .append('text')
+      .attr('class', 'legend-title')
+      .attr("x", legendX + 20)
+      .attr("y", height - 740)
+      .attr("dy", "0.8em") //place text one line *below* the x,y point
+      .text(function (d, i) {
+        return d;
+      });
+
+    // var gNode = g.node().getBBox();
+    // console.log(gNode);
+    // g.attr("transform", function (d) {
+    //   var x = (width-gNode.width)/2;
+    //   var y = (height-gNode.height)/2;
+    //   return "translate(" + x/2 + "," + y/2 + ")";
+    // });
 
     g.append("g").attr("class", 'circle-group');
     // var localDistrictSettlement = $.extend(true, [], districtSettlement);
