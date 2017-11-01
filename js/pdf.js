132e7b805c2ba89073c9ff52c46ffc5add130fa5
Peter Vysotskiy <vysotskiy.petr@gmail.com>
2017-11-01 17:28:37 +0700

    Crop image

--- js/pdf.js
+++ js/pdf.js
@@ -13,18 +13,44 @@ function _printPageHeader(doc, date) {
 	})
 }
 
-function printFilter(doc, filter, x, y) {
+function _printFilter(doc, filter, x, y) {
 	doc.setFontSize(8);
 	doc.text(x, y, 'Filters Applied');
 	doc.setFontSize(12);
 	doc.text(x, y + 5, filter.name);
-	filter.values.forEach(function(value, index) {
+	filter.values.forEach(function (value, index) {
 		doc.text(x, y + 14 + index * 5, value.key);
 	})
 }
 
-function generatePdf(map, dataset, filters, lastModified) {
-	console.log(filters);
+function makeRectangularCanvas(oldCanvas, callback) {
+	var canvas = document.createElement('canvas');
+	var context = canvas.getContext('2d');
+	var imageObj = new Image();
+	dx = oldCanvas.width - oldCanvas.height;
+
+	canvas.width = oldCanvas.height;
+	canvas.height = oldCanvas.height;
+
+	imageObj.src = oldCanvas.toDataURL('image/png');
+	imageObj.onload = function () {
+		// draw cropped image
+		var sourceX = dx / 2;
+		var sourceY = 0;
+		var sourceWidth = oldCanvas.width - dx;
+		var sourceHeight = oldCanvas.height;
+		var destWidth = sourceWidth;
+		var destHeight = sourceHeight;
+		var destX = canvas.width / 2 - destWidth / 2;
+		var destY = canvas.height / 2 - destHeight / 2;
+
+		context.drawImage(imageObj, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
+
+		callback(canvas);
+	};
+}
+
+function _makeMapImage(map, callback) {
 	// Generate map image first
 	leafletImage(map, function (err, canvas) {
 		var svg = document.querySelector('svg');
@@ -33,45 +59,57 @@ function generatePdf(map, dataset, filters, lastModified) {
 		// Make png from SVG D3 map
 		svgAsPngUri(svg, {}, function (uri) {
 			var drawing = new Image();
-			drawing.src = uri; // can also be a remote URL e.g. http://
+			drawing.src = uri;
 			drawing.onload = function () {
 				context.drawImage(drawing, 0, 0);
 
-				var dataURL = canvas.toDataURL('image/png');
-
-				var doc = new jsPDF();
-				var ratio = canvas.height / canvas.width;
-				doc.setFont('helvetica', 'normal');
-
-				_printPageHeader(doc, lastModified);
-
-				doc.addImage(dataURL, 'PNG', 0, 80, 220, ratio * 220);
-				
-				filters.forEach(function (filter, index) {
-					printFilter(doc, filter, 5 + index * 50, 220);
+				makeRectangularCanvas(canvas, function (canvas) {
+					callback(canvas);
 				});
-				
-				doc.addPage();
-
-				var columns = [
-					{ title: 'District Name', dataKey: 'District' },
-					{ title: 'Actor Name', dataKey: 'Actor Name' },
-					{ title: 'Settlement Name', dataKey: 'Settlement Name' },
-					{ title: 'Sector', dataKey: 'Sector' },
-				];
-
-				doc.autoTable(columns, dataset, {
-					margin: { top: 60 },
-					addPageContent: function (data) {
-						_printPageHeader(doc, lastModified);
-					}
-				});
-
-				doc.save();
 			};
+		});
+	});
+}
 
+function generatePdf(map, dataset, filters, lastModified) {
+	var IMAGE_SIZE = 170;
+	var DOC_WIDTH = 210;
+
+	_makeMapImage(map, function (canvas) {
+		var doc = new jsPDF();
+		var ratio = canvas.height / canvas.width;
+		doc.setFont('helvetica', 'normal');
+
+		_printPageHeader(doc, lastModified);
+
+		var image = canvas.toDataURL('image/png');
+		var x = (DOC_WIDTH - IMAGE_SIZE)/2;
+		var y = 50;
+		var width = IMAGE_SIZE;
+		var height = IMAGE_SIZE * ratio;
+		doc.addImage(image, 'PNG', x, y, width, height, '', 'fast');
+
+		filters.forEach(function (filter, index) {
+			_printFilter(doc, filter, 5 + index * 50, 220);
 		});
 
-	});
+		doc.addPage();
+
+		var columns = [
+			{ title: 'District Name', dataKey: 'District' },
+			{ title: 'Actor Name', dataKey: 'Actor Name' },
+			{ title: 'Settlement Name', dataKey: 'Settlement Name' },
+			{ title: 'Sector', dataKey: 'Sector' },
+		];
+
+		doc.autoTable(columns, dataset, {
+			margin: { top: 60 },
+			addPageContent: function (data) {
+				_printPageHeader(doc, lastModified);
+			}
+		});
+
+		doc.save();
+	})
 }
 
